5fcb2c498da047e25cc39ac58a1ddf5874c321b6
Gabriel Raimondo <46075753+GabrielRai@users.noreply.github.com>
2024-11-28 05:07:27 +0100

    Created OrderItemUnitTest-file.

---
+++ WebShopTests/UnitTests/OrderItemUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Repository.Repositories.OrderItems;
+
+namespace WebShopTests.UnitTests
+{
+    public class OrderItemUnitTest
+    {
+        private readonly Mock<IOrderItemRepository> _mockOrderRepository;
+        public OrderItemUnitTest()
+        {
+            _mockOrderRepository = new Mock<IOrderItemRepository>();
+        }
+
+    }
+}
